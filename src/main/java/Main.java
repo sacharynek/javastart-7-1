a10562b1bea3db4d969d79a4f06d0f6e1553242b
tomaszlach86 <tomaszlach86@gmail.com>
2019-05-24 11:32:17 +0200

    updated styling

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,13 +7,13 @@ public class Main {
 
         String[] names = new String[5];
 
-        for(int i =0; i<names.length; i++){
-            System.out.println("Podaj imie numer "+(i+1));
+        for (int i = 0; i < names.length; i++) {
+            System.out.println("Podaj imie numer " + (i + 1));
             names[i] = sc.nextLine();
         }
-        
-        for(int i = names.length-1; i>-1; i--){
-            System.out.println("imie numer "+(i+1)+ " to "+names[i]);
+
+        for (int i = names.length - 1; i > -1; i--) {
+            System.out.println("imie numer " + (i + 1) + " to " + names[i]);
         }
 
     }
